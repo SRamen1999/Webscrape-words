5ea7e800c74a7ed39d28747b94e8f3e5e155940d
SRamen1999 <56932664+SRamen1999@users.noreply.github.com>
2020-08-14 16:55:19 -0400

    New update
    Can bypass 403 forbidden

--- TestProject/main.py
+++ TestProject/main.py
@@ -21,6 +21,7 @@ for image in images:
     image_src = image["src"]
 
     print(image_src)
+    #next three lines bypass 403 forbidden
     opener = urllib.request.build_opener()
     opener.addheaders = [('User-Agent','Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1941.0 Safari/537.36')]
     urllib.request.install_opener(opener)
