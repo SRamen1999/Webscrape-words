30a91d18e049275b39b7ea01f409dde11f4050c0
SRamen1999 <56932664+SRamen1999@users.noreply.github.com>
2020-08-14 16:52:33 -0400

    New update
    It can bypass 403 forbidden

--- TestProject/main.py
+++ TestProject/main.py
@@ -6,35 +6,36 @@ import urllib.request
 
 
 #image scraping
-url = "https://narutoshippuden-manga.com/manga/naruto-chapter-670/"
-source_code = rq.get(url)
+
+response = input("Enter url: ")
+source_code = rq.get(response)
 plain_text = source_code.content
 soup = BeautifulSoup(plain_text, 'html.parser')
 print(soup.title)
-
-
 images = soup.find_all("img")
 print(images)
-
 number = 0
 
 #downloads the images
 for image in images:
     image_src = image["src"]
-    print(image_src)
 
+    print(image_src)
+    opener = urllib.request.build_opener()
+    opener.addheaders = [('User-Agent','Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1941.0 Safari/537.36')]
+    urllib.request.install_opener(opener)
     urllib.request.urlretrieve(image_src, str(number))
     number += 1
 
 #prints the words from the pictures
 
 pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
-for num in range(len(image_src)-1):
+for num in range(len(images)):
     print("Page " + str(num))
     img = cv2.imread(str(num))
     img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
     text = pytesseract.image_to_string(img)
     print(text)
     cv2.imshow('Result',img)
-    cv2.waitKey(1)
+    cv2.waitKey(0)
 
